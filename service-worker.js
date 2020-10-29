127fa116a8b74a59431c89569f0e1d9657763bf7
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-29 19:43:00 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.d61ea4fecc21dd47f6da60565b9d3b19.js"
 );
 
 self.addEventListener('message', (event) => {
